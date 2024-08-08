d8cd0a050d507c09b547d56ed0e964f66bca1c56
Andrew Huffman <ahuffman@cedarville.edu>
2024-08-08 18:56:43 -0400

    Make query classes take json data rather than csv

--- text_query.py
+++ text_query.py
@@ -6,7 +6,7 @@ import helpers
 class TextQuery:
     """A superclass for different kinds of searches over the text of the NT.
     Should be extended with unique fields and a search() function."""
-    def search(self, dataset: list[list[str]]) -> list[list[str]]:
+    def search(self, dataset: list[dict[str, str]]) -> list[dict[str, str]]:
         """Searches through the given dataset with the fields set up in the class."""
         raise NotImplementedError('Call search() from a subclass!')
 
@@ -20,23 +20,23 @@ class LexemeQuery(TextQuery):
         self.number = None
         self.gender = None
 
-    def search(self, dataset: list[list[str]]) -> list[list[str]]:
+    def search(self, dataset: list[dict[str, str]]) -> list[dict[str, str]]:
         """Searches for the given lexeme."""
         # Create a sublist of rows with the given lexeme.
-        lexeme_rows = [row for row in dataset if helpers.get_row_val('lexeme', row) == self.lexeme]
+        lexeme_rows = [row for row in dataset if row['lexeme'] == self.lexeme]
 
         # Further limit based upon other fields if provided.
         if self.case is not None:
             lexeme_rows = [row
                            for row in lexeme_rows
-                           if self.case in helpers.interpret_rmac_code(helpers.get_row_val('rmac', row))]
+                           if self.case in helpers.interpret_rmac_code(row['rmac'])]
         if self.number is not None:
             lexeme_rows = [row
                            for row in lexeme_rows
-                           if self.number in helpers.interpret_rmac_code(helpers.get_row_val('rmac', row))]
+                           if self.number in helpers.interpret_rmac_code(row['rmac'])]
         if self.gender is not None:
             lexeme_rows = [row
                            for row in lexeme_rows
-                           if self.gender in helpers.interpret_rmac_code(helpers.get_row_val('rmac', row))]
+                           if self.gender in helpers.interpret_rmac_code(row['rmac'])]
 
         return lexeme_rows
