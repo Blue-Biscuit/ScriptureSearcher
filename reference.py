b14ff101784d852f48f4b18d31103bb085a8318c
Andrew Huffman <ahuffman9000@gmail.com>
2024-08-31 16:51:00 -0400

    Class to parse compound references.

--- reference.py
+++ reference.py
@@ -1,6 +1,18 @@
 """Defines types related to handling chapter/verse references."""
 import re
 
+def _split_verse_into_num_and_char(verse_string: str) -> (str, str):
+    """Splits the given verse number (and ONLY verse number) into a number and char."""
+    char_idx = -1
+    for i, c in enumerate(verse_string):
+        if not c.isdigit():
+            char_idx = i
+            break
+    verse_letter = verse_string[char_idx:]
+    verse_part = verse_string[0:char_idx]
+
+    return verse_part, verse_letter
+
 class Reference:
     """A single chapter/verse reference (so no 1.1-2)."""
     def __init__(self, chapter_number: int, verse_number: int = None, verse_letter: str = None):
@@ -42,13 +54,7 @@ class Reference:
 
             # If there are characters in the verse part, we need to separate that out from the integer part.
             if re.match('.*[a-z]', verse_part):
-                char_idx = -1
-                for i, c in enumerate(verse_part):
-                    if not c.isdigit():
-                        char_idx = i
-                        break
-                verse_letter = verse_part[char_idx:]
-                verse_part = verse_part[0:char_idx]
+                verse_part, verse_letter = _split_verse_into_num_and_char(verse_part)
 
             result = Reference(int(chapter_part), int(verse_part), verse_letter)
 
@@ -56,3 +62,60 @@ class Reference:
             result = Reference(int(string))
 
         return result
+
+class CompoundReference:
+    """A reference range (so 1.1-2)."""
+    def __init__(self, from_ref: Reference, to_ref: Reference):
+        # TODO: check in here to ensure that the references make sense (so from < to)
+
+        self.from_ref = from_ref
+        self.to_ref = to_ref
+
+    @staticmethod
+    def is_compound_reference(string: str) -> bool:
+        """True if the string is the correct format for a compound reference."""
+        # Ensure that '-' is in the string (it must be for this to be a compound reference)
+        if '-' not in string:
+            return False
+
+        # Check if the first half is a reference.
+        dash_idx = string.index('-')
+        if not Reference.is_reference(string[0:dash_idx]):
+            return False
+
+        # Check that the second half is just an integer or if it's another reference.
+        after_dash = string[dash_idx+1:]
+        if not (re.match('[0-9]+[a-z]?', after_dash) or Reference.is_reference(after_dash)):
+            return False
+
+        return True
+
+    @staticmethod
+    def from_str(string: str) -> 'CompoundReference':
+        """Parses the reference range."""
+        # Assert that this is of the correct format.
+        string = string.strip()
+        if not CompoundReference.is_compound_reference(string):
+            raise ValueError(f'Not a valid compound reference: {string}')
+
+        # Parse everything up until the '-' as a regular reference.
+        dash_idx = string.index('-')
+        from_ref = Reference.from_str(string[0:dash_idx])
+
+        # Parse after the dash, either as just a verse specifier or as a full reference.
+        after_dash = string[dash_idx+1:]
+        if re.match('[0-9]+[a-z]?', after_dash):
+            if re.match('.*[a-z]', after_dash):
+                verse_number, verse_char = _split_verse_into_num_and_char(after_dash)
+            else:
+                verse_number = after_dash
+                verse_char = None
+            to_ref = Reference(from_ref.chapter_number, int(verse_number), verse_char)
+
+        elif Reference.is_reference(after_dash):
+            to_ref = Reference.from_str(after_dash)
+
+        else:
+            raise ValueError(f'Not a valid compound reference: {string}')
+
+        return CompoundReference(from_ref, to_ref)
